c1bde4c448aceed280a554702f89401b69b8a36c
Tom Matos <toms.matos@gmail.com>
2018-05-22 16:20:50 +0000

    delete

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -50,8 +50,12 @@ var routes = function(Book) {
             req.book.genre = req.body.genre;
             req.book.read = req.body.read;
                     
-            req.book.save();
-            res.json(req.book);
+            req.book.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else
+                    res.json(req.book);
+            });
         })
         .patch(function(req, res){
             if(req.body.id)
@@ -65,6 +69,14 @@ var routes = function(Book) {
                 else
                     res.json(req.book);
             });
+        })
+        .delete(function(req, res) {
+            req.book.remove(function(err){
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.status(204).send("Deleted");
+            })
         });
     return bookRouter;
 };
