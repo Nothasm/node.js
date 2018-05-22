208e658c489c13bb463275edc585711b524354f3
Tom Matos <toms.matos@gmail.com>
2018-05-22 00:59:28 +0000

    Learning

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://' + process.env.IP + '/bookAPI');
+var Book = require('./models/bookModel');
+var app = express()
+var port = process.env.PORT;
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req, res){
+        var query = {};
+        
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        
+        Book.find(query, function(err, books){
+            if (err)
+                console.log(err);
+            else
+                res.json(books);
+        });
+    });
+
+bookRouter.route('/Books/:bookId')
+    .get(function(req, res){
+        Book.findById(req.params.bookId, function(err, books){
+            if (err)
+                console.log(err);
+            else
+                res.json(books);
+        });
+    });
+
+app.use('/api', bookRouter);
+
+
+
+
+app.get('/', function(req, res){
+    res.send('welcome to my API');
+});
+
+app.listen(port, function(){
+    console.log('Running on port: ' + port);
+})
